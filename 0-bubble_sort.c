435cf62a1a7d24fb34341f44869d5ce11ba58197
davekilonzo <davidkilonzo1998@gmail.com>
2024-06-06 02:10:48 -0700

    correction 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,7 +24,7 @@ void swap_int(int *a, int *b)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, k;
-	
+
 	for (i = 0; i < size - 1; ++i)
 	{
 		for (j = 0; j < size - i - 1; ++j)
@@ -41,4 +41,3 @@ void bubble_sort(int *array, size_t size)
 		}
 	}
 }
-
